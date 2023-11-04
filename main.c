0a7a6b0e88883c53702a31c31a4a2548f1223ba5
rojberr <rojberr@outlook.com>
2023-11-04 13:16:42 +0100

    feat(otaku): Add headers and MPI_Init

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
+#include <mpi.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
  int main(int argc, char **argv) {
     if (argc < 4) {
         cout << "Input more than 3 args " << int(argc)-1 << '\n';
         return -1;
     }
+
+    MPI_Init(&argc, &argv);
 
     return 0;
 }
